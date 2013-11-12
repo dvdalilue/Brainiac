9ad32316601e69de124b65b5604edb6fd1839d7e
David Lilue <dvdalilue@debian.David>
2013-11-12 15:12:17 -0500

    Se ha trabajado en otro repo por lo que este archivo a sido cambiado bastante. Para otros commits y versiones, revisar en bitbucket

--- tokens.rb
+++ tokens.rb
@@ -1,28 +1,9 @@
 
+###########################
+#Enumeracion de los Tokens#
+###########################
 
-class FraseS
-
-  attr_accessor :text, :line, :column
-
-  def initialize(text, line, column)
-    @text = text
-    @line = line
-    @column = column
-  end
-
-  
-end
-
-class ErrorLexicos < FraseS
-
-  def to_s
-    "Error: Caracter inesperado "#{@text}" en la fila #{@line}, columna #{@column}"
-  end
-
-end
-
-
-tokens = {
+$tokens = {
 
   'Coma'            =>  /\A,/            ,
   'Punto'           =>  /\A./            , 
@@ -51,22 +32,156 @@ tokens = {
   'Concat'          =>  /\A\&/           , 
   'Inspeccion'      =>  /\A\#/           ,
   'Asignacion'      =>  /\A\:=/          ,
-  'Id'              =>  /\A[0-9a-zA-Z_]*/,
+  'Ident'           =>  /\A[0-9a-zA-Z_]*/,
+  'Num'             =>  /\A[0-9]*/       ,
+  'Cinta'           =>  /\A\{[+-<>.,]*}/ ,
 
 }
 
-reserved_words = %w(declare execute done read while do if else end)
+reserved_words = %w(declare execute done read while do if else end at tape to)
 
 reserved_words.each do |s|
-  tokens[s.capitalize] = /\A#{s}\b/
+  $tokens[s.capitalize] = /\A#{s}\b/
 end
+
+###########################
+#Declaracion de las clases#
+###########################
+
+class PhraseS
+
+  attr_accessor :text, :line, :column
+
+  def initialize(text, line, column)
+    @text = text
+    @line = line
+    @column = column
+  end
+
+  
+end
+
+class LexicographError < PhraseS
+
+  def initialize(text, line, column)
+    super(text, line, column)
+  end
+
+  def to_s
+    "Error: Caracter inesperado "#{@text}" en la fila #{@line}, columna #{@column}"
+  end
+
+end
+
+class Token < PhraseS
+
+  def initialize(text, line, column,regex)
+    super(text, line, column)
+    @regex = regex
+  end
+
+  def to_s
+    "#{self.class.name} #{if self.class.name.eql?(TkIdent) then (" + @text + ") end}"
+  end
+end
+
+class Lexer
+
+  def initialize(input)
+    @tokens = []
+    @errors = []
+    @input = input
+    @line = 0
+    @column = 0
+    @comment = 0
+  end
+
+  def lex_tokens
+    @tokens
+  end
+
+  def lex_errors
+    @errors
+  end
+
+  def lex_ignore(length)
+    @input = @input[length..@input.length]
+  end
+
+  def tokenize(somephrase, length)
+    if $tokens.has_key?(somephrase.capitalize)
+      puts "Es palabra reservada -- #{somephrase}"
+    elsif somephrase =~ /\A\$-.*/
+      @comment = 1
+      puts "Comentario inicia"
+    else
+      puts somephrase
+    end
+  end
+
+  def lex_catch
+
+    return false if @input.eql?(nil)
+
+    @input.match(/\A\s*/)
+    lex_ignore($&.length)
+
+    @input.match(/\A[\w\p{punct}]*\s/)
+
+    if $&.eql?nil
+      lex_ignore(1)
+    else
+
+      if @comment == 0
+        tokenize(@input[0..($&.length-2)],$&.length-2)
+      else
+        if $&.include?("-$")
+          @comment = 0
+          puts "Comentario Cierra"
+        end
+      end
+      lex_ignore($&.length)
+    end
+  end
+end
+
+#######################################
+#Declaracion de clases para cada token#
+#######################################
+
+$tokens.each do |id,regex|
+
+  newclass = Class::new(Token) do
+
+    @regex = regex
+    
+    def initialize(text, line, column)
+      @text = text
+      @line = line
+      @column = column
+    end
+  end
+
+  Object::const_set("Tk#{id}", newclass)
+
+end
+
+
+###############################
+#Definicion del Main del Lexer#
+###############################
 
 def main
   
   input = File::read(ARGV[0])
-  
-  puts input
 
+  goon = true
+
+  lexer = Lexer::new (input)
+
+  while (goon) do
+    goon = lexer.lex_catch
+  end
 end
 
 main
