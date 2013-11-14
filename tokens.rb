130a374a57d2b187bec48d867fceb622b2744013
David Lilue <dvdalilue@debian.David>
2013-11-14 04:32:06 -0500

    Ya esta realizando la creacion de cada token, faltaria la impresion

--- tokens.rb
+++ tokens.rb
@@ -5,36 +5,36 @@
 
 $tokens = {
 
-  'Coma'            =>  /\A,/            ,
-  'Punto'           =>  /\A./            , 
-  'PuntoYComa'      =>  /\A;/            , 
-  'ParAbre'         =>  /\A\(/           , 
-  'ParCierra'       =>  /\A\)/           , 
-  'CorcheteAbre'    =>  /\A\[/           , 
-  'CorcheteCierra'  =>  /\A\]/           , 
-  'LlaveAbre'       =>  /\A\{/           , 
-  'LlaveCierra'     =>  /\A\}/           , 
-  'Type'            =>  /\A\:\:/         , 
-  'Menos'           =>  /\A\-/           ,
-  'Mas'             =>  /\A\+/           ,
-  'Mult'            =>  /\A\*/           , 
-  'Div'             =>  /\A\/(?!=)/      , 
-  'Mod'             =>  /\A\%/           , 
-  'Conjuncion'      =>  /\A\/\\/         , 
-  'Disyuncion'      =>  /\A\\\//         , 
-  'Negacion'        =>  /\A\~/           , 
-  'Menor'           =>  /\A\<(?!=)/      , 
-  'MenorIgual'      =>  /\A\<=/          , 
-  'Mayor'           =>  /\A\>(?!=)/      , 
-  'MayorIgual'      =>  /\A\>=/          , 
-  'Igual'           =>  /\A\=/           , 
-  'Desigual'        =>  /\A\/=/          , 
-  'Concat'          =>  /\A\&/           , 
-  'Inspeccion'      =>  /\A\#/           ,
-  'Asignacion'      =>  /\A\:=/          ,
-  'Ident'           =>  /\A[0-9a-zA-Z_]*/,
-  'Num'             =>  /\A[0-9]*/       ,
-  'Cinta'           =>  /\A\{[+-<>.,]*}/ ,
+  'Coma'            =>  /\A,/                     ,
+  'Punto'           =>  /\A\./                     , 
+  'PuntoYComa'      =>  /\A;/                     , 
+  'ParAbre'         =>  /\A\(/                    , 
+  'ParCierra'       =>  /\A\)/                    , 
+  'CorcheteAbre'    =>  /\A\[/                    , 
+  'CorcheteCierra'  =>  /\A\]/                    , 
+  'LlaveAbre'       =>  /\A\{/                    , 
+  'LlaveCierra'     =>  /\A\}/                    , 
+  'Type'            =>  /\A\:\:/                  , 
+  'Menos'           =>  /\A\-/                    ,
+  'Mas'             =>  /\A\+/                    ,
+  'Mult'            =>  /\A\*/                    , 
+  'Div'             =>  /\A\/(?!=)/               , 
+  'Mod'             =>  /\A\%/                    , 
+  'Conjuncion'      =>  /\A\/\\/                  , 
+  'Disyuncion'      =>  /\A\\\//                  , 
+  'Negacion'        =>  /\A\~/                    , 
+  'Menor'           =>  /\A\<(?!=)/               , 
+  'MenorIgual'      =>  /\A\<=/                   , 
+  'Mayor'           =>  /\A\>(?!=)/               , 
+  'MayorIgual'      =>  /\A\>=/                   , 
+  'Igual'           =>  /\A\=/                    , 
+  'Desigual'        =>  /\A\/=/                   , 
+  'Concat'          =>  /\A\&/                    , 
+  'Inspeccion'      =>  /\A\#/                    ,
+  'Asignacion'      =>  /\A\:=/                   ,
+  'Ident'           =>  /\A[a-zA-Z_][0-9a-zA-Z_]*/,
+  'Num'             =>  /\A[0-9]*/                ,
+  'Cinta'           =>  /\A\{[+-<>.,]*}/          ,
 
 }
 
@@ -75,10 +75,11 @@ end
 
 class Token < PhraseS
 
-  def initialize(text, line, column,regex)
-    super(text, line, column)
-    @regex = regex
+  class << self
+    attr_accessor :regex
   end
+
+  #attr_accessor :line, :column
 
   def to_s
     "#{self.class.name} #{if self.class.name.eql?(TkIdent) then (" + @text + ") end}"
@@ -106,16 +107,47 @@ class Lexer
 
   def lex_ignore(length)
     @input = @input[length..@input.length]
+    @column += length
+  end
+
+  def lookclass(xclass)
+    return Object.const_get(xclass)
   end
 
   def tokenize(somephrase, length)
+
+    newclasstk = LexicographError
+    newtoktext = somephrase
+
     if $tokens.has_key?(somephrase.capitalize)
-      puts "Es palabra reservada -- #{somephrase}"
+      newclasstk = "Tk" + "#{somephrase.capitalize}"
     elsif somephrase =~ /\A\$-.*/
       @comment = 1
-      puts "Comentario inicia"
+      newclasstk = "comment"
     else
-      puts somephrase
+      $tokens.each { |key, value|
+        if @input =~ value
+          newclasstk = "Tk" + "#{key}"
+          newtoktext = $& #Previous match
+          break
+        end
+      }
+    end
+
+    if @comment == 0
+      if newclasstk == LexicographError 
+        puts "Error lexicografico -- #{somephrase}"
+      end
+      
+      newclasstk = lookclass(newclasstk)
+      puts newclasstk.regex.inspect
+      newtk = newclasstk.new(newtoktext,@line,@colunm)
+      
+      if newtk.is_a? LexicographError
+        @errors << newtk
+      else
+        @tokens << newtk
+      end  
     end
   end
 
@@ -130,17 +162,17 @@ class Lexer
 
     if $&.eql?nil
       lex_ignore(1)
+      @line += 1
     else
-
+      mlength = $&.length
       if @comment == 0
         tokenize(@input[0..($&.length-2)],$&.length-2)
       else
-        if $&.include?("-$")
+        if $& =~ /\A\.*\-\$/
           @comment = 0
-          puts "Comentario Cierra"
         end
       end
-      lex_ignore($&.length)
+      lex_ignore(mlength)
     end
   end
 end
